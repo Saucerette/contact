5c08886d5e58b414547cdf4f03f8f6e46d93adb2
Tim Rodger <tim.rodger@gmail.com>
2015-06-29 23:19:28 +0100

    Removes old code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-var //express = require('express'),
-    //bodyParser = require('body-parser'),
-    logger = require('./lib/logger'),
-    //routes = require('./routes')
+var logger = require('./lib/logger'),
     web_router = require('./lib/web_router');
 
 web_router.running();
 web_router.connected();
-
-//var PORT = process.env.PORT || 80;
-//var app = express();
-//
-//app.use(bodyParser.json({limit: '1024kb'}));
-//app.use('/', routes);
-//app.listen(PORT);
 
 logger.info('Contact service running');
